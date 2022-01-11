1a0c2b8b1d965ab8dbf31bad0cf20934a7456d74
Evelyn Velez <67163723+evs09@users.noreply.github.com>
2022-01-11 08:39:38 -0500

    Assignment-4
    Answers to assignment 4 added

--- script.js
+++ script.js
@@ -30,3 +30,41 @@ for(let i = 0; i < items.length; i++){
 //#12
 let rem = document.getElementsByClassName("footer")
 rem[0].remove();
+
+//Assignment 4 script
+//#1
+let btn1 = document.getElementById("btn-1");
+let btn2 = document.getElementById("btn-2");
+let fightp = document.getElementById("fight-text");
+
+btn1.addEventListener("click", function(){
+	fightp.innerHTML = "I'm right";
+
+});
+
+btn2.addEventListener("click", function(){
+	fightp.innerHTML = "No, I'm right";
+
+});
+
+
+//#2
+let el = document.getElementById("hoverp");
+
+el.addEventListener("mouseover", function(){
+	alert("Hey, I told you not to hover over me!");
+
+});
+
+//#3
+let submit = document.getElementById("submit-btn");
+
+submit.addEventListener("click", function(){
+	let pw = document.getElementById("pw").value;
+	if(pw == "12345678"){
+		document.getElementById("formText").innerHTML = "Logged In";
+	}
+	else{
+		alert("Incorrect Password");
+	}
+})
